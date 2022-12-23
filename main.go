1ad62cbb8aea3788c58cff7a41195ee47324a5f8
Abhijith-TR <abhijith0909@gmail.com>
2022-12-23 23:50:03 +0530

    Add subjects

--- main.go
+++ main.go
@@ -14,6 +14,10 @@ import (
 	"github.com/xuri/excelize/v2"
 )
 
+// Functionality to be added
+// 2. Add students
+// 3. Allow user to specify which columns contain what values
+
 var env map[string]string
 
 func main() {
@@ -47,10 +51,6 @@ func main() {
 			Value: "",
 		},
 		&cli.BoolFlag{
-			Name: "createuser",
-			Value: false,
-		},
-		&cli.BoolFlag{
 			Name: "createsubject",
 			Value: false,
 		},
@@ -65,78 +65,17 @@ func main() {
 			Action: handleLogin,
 		},
 		{
-			Name: "insert",
-			Aliases: []string{"i"},
+			Name: "binsert",
+			Aliases: []string{"bi"},
 			Usage: "Insert Records from .xlsx file",
 			Flags: insertFlags,
-			Action: func(ctx *cli.Context) error {
-				fileName := string(ctx.Args().Get(0))
-				sheetName := string(ctx.Args().Get(1))
-				if ctx.NArg() == 0 {
-					return fmt.Errorf("please provide file name")
-				}
-				f, err := excelize.OpenFile(fileName)
-				if err != nil {
-					return err
-				}
-				defer func() {
-					if err := f.Close(); err != nil {
-						fmt.Println("File not closed")
-					}
-				}()
-				rows, err := f.GetRows(sheetName)
-				if err != nil {
-					return err
-				}
-				for idx, row := range rows {
-					postBody, err := json.Marshal(map[string]string {
-						"entryNumber": row[0],
-						"grade": row[1],
-						"subjectCode": func() string {
-							if len(ctx.String("subjectcode")) == 0 {
-								return row[2]
-							} else {
-								return ctx.String("subjectcode")
-							}
-						}(),
-						"semester": func() string {
-							if len(ctx.String("semester")) == 0 {
-								return row[3]
-							} else {
-								return ctx.String("semester")
-							}
-						}(),
-					})
-					responseBody := bytes.NewBuffer(postBody)
-					if err != nil {
-						f.SetCellValue(sheetName, "E"+fmt.Sprint(idx+1), "Failed")
-						return err
-					}
-					client := &http.Client{}
-					req, err := http.NewRequest("POST", env["WEBSITE"]+"/admin/records/single", responseBody)
-					if err != nil {
-						f.SetCellValue(sheetName, "E"+fmt.Sprint(idx+1), "Failed")
-						return err
-					}
-					req.Header.Add("Authorization", "Bearer " + env["TOKEN"])
-					req.Header.Set("Content-Type", "application/json")
-					res, err := client.Do(req)
-					if err != nil {
-						f.SetCellValue(sheetName, "E"+fmt.Sprint(idx+1), "Failed")
-						return err
-					}
-					defer res.Body.Close()
-					var data map[string]interface{}
-					err = json.NewDecoder(res.Body).Decode(&data)
-					if err != nil {
-						f.SetCellValue(sheetName, "E"+fmt.Sprint(idx+1), "Failed")
-						return err
-					}
-					f.SetCellValue(sheetName, "E"+fmt.Sprint(idx+1), data["msg"])
-				}
-				f.SaveAs(fileName)
-				return nil
-			},
+			Action: handlebInsert,
+		},
+		{
+			Name: "sinsert",
+			Aliases: []string{"si"},
+			Usage: "Insert subject into database",
+			Action: handlesInsert,
 		},
 	}
 
@@ -198,3 +137,104 @@ func handleLogin(ctx *cli.Context) error {
 	return nil
 }
 
+func handlebInsert(ctx *cli.Context) error {
+	fileName := string(ctx.Args().Get(0))
+	sheetName := string(ctx.Args().Get(1))
+	if ctx.NArg() == 0 {
+		return fmt.Errorf("please provide file name")
+	}
+	f, err := excelize.OpenFile(fileName)
+	if err != nil {
+		return fmt.Errorf("could not open .xlsx file")
+	}
+	defer func() {
+		if err := f.Close(); err != nil {
+			fmt.Println("File not closed")
+		}
+	}()
+	rows, err := f.GetRows(sheetName)
+	if err != nil {
+		return fmt.Errorf("could not read .xlsx file")
+	}
+	for idx, row := range rows {
+		postBody, err := json.Marshal(map[string]string {
+			"entryNumber": row[0],
+			"grade": row[1],
+			"subjectCode": func() string {
+				if len(ctx.String("subjectcode")) == 0 {
+					return row[2]
+				} else {
+					return ctx.String("subjectcode")
+				}
+			}(),
+			"semester": func() string {
+				if len(ctx.String("semester")) == 0 {
+					return row[3]
+				} else {
+					return ctx.String("semester")
+				}
+			}(),
+		})
+		if err != nil {
+			f.SetCellValue(sheetName, "E"+fmt.Sprint(idx+1), "Failed")
+			return fmt.Errorf("could not build json body")
+		}
+		responseBody := bytes.NewBuffer(postBody)
+		client := &http.Client{}
+		req, err := http.NewRequest("POST", env["WEBSITE"]+"/admin/records/single", responseBody)
+		if err != nil {
+			f.SetCellValue(sheetName, "E"+fmt.Sprint(idx+1), "Failed")
+			return fmt.Errorf("request formation failed")
+		}
+		req.Header.Add("Authorization", "Bearer " + env["TOKEN"])
+		req.Header.Set("Content-Type", "application/json")
+		res, err := client.Do(req)
+		if err != nil {
+			f.SetCellValue(sheetName, "E"+fmt.Sprint(idx+1), "Failed")
+			return fmt.Errorf("request failed")
+		}
+		defer res.Body.Close()
+		var data map[string]interface{}
+		err = json.NewDecoder(res.Body).Decode(&data)
+		if err != nil {
+			f.SetCellValue(sheetName, "E"+fmt.Sprint(idx+1), "Failed")
+			return fmt.Errorf("could not decode response")
+		}
+		f.SetCellValue(sheetName, "E"+fmt.Sprint(idx+1), data["msg"])
+	}
+	f.SaveAs(fileName)
+	return nil
+}
+
+func handlesInsert(ctx *cli.Context) error {
+	if ctx.NArg() == 0 {
+		return fmt.Errorf("enter valid subject")
+	}
+	postBody, err := json.Marshal(map[string]string {
+		"subjectCode": ctx.Args().Get(0),
+		"subjectName": ctx.Args().Get(1),
+	})
+	if err != nil {
+		return fmt.Errorf("could not create json object")
+	}
+	responseBody := bytes.NewBuffer(postBody)
+	client := &http.Client{}
+	req, err := http.NewRequest("POST", env["WEBSITE"]+"/admin/records", responseBody)
+	if err != nil {
+		return fmt.Errorf("request formulation failed")
+	}
+	req.Header.Add("Authorization", "Bearer " + env["TOKEN"])
+	req.Header.Set("Content-Type", "application/json")
+	res, err := client.Do(req)
+	if err != nil {
+		return fmt.Errorf("request failed")
+	}
+	defer res.Body.Close()
+	var data map[string]interface{}
+	err = json.NewDecoder(res.Body).Decode(&data)
+	if err != nil {
+		return fmt.Errorf("could not decode response")
+	}
+	fmt.Println(data["msg"])
+	return nil
+}

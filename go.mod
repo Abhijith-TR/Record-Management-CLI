0a3cda890436d27596550aeebb92fee2f634bd59
Abhijith-TR <abhijith0909@gmail.com>
2022-12-23 22:43:23 +0530

    Basic Insert

--- go.mod
+++ go.mod
@@ -6,7 +6,16 @@ require github.com/urfave/cli/v2 v2.23.7 // direct
 
 require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
-	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/joho/godotenv v1.4.0 // direct
+	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
+	github.com/richardlehane/mscfb v1.0.4 // indirect
+	github.com/richardlehane/msoleps v1.0.3 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	github.com/xuri/efp v0.0.0-20220603152613-6918739fd470 // indirect
+	github.com/xuri/excelize/v2 v2.6.1 // indirect
+	github.com/xuri/nfp v0.0.0-20220409054826-5e722a1d9e22 // indirect
+	golang.org/x/crypto v0.0.0-20220817201139-bc19a97f63c8 // indirect
+	golang.org/x/net v0.0.0-20220812174116-3211cb980234 // indirect
+	golang.org/x/text v0.3.7 // indirect
 )
--- main.go
+++ main.go
@@ -6,16 +6,19 @@ import (
 	"fmt"
 	"os"
 	"strings"
-
 	"net/http"
 	"bufio"
 
 	"github.com/joho/godotenv"
 	"github.com/urfave/cli/v2"
+	"github.com/xuri/excelize/v2"
 )
 
+var env map[string]string
+
 func main() {
-	env, err := godotenv.Read(".env")
+	var err error
+	env, err = godotenv.Read(".env")
 	if err != nil {
 		fmt.Println("Could not load .env")
 		return
@@ -34,46 +37,104 @@ func main() {
 	app.UsageText = "IRMS - CLI"
 	app.EnableBashCompletion = true
 
+	insertFlags := []cli.Flag{
+		&cli.StringFlag{
+			Name: "semester",
+			Value: "",
+		},
+		&cli.StringFlag{
+			Name: "subjectcode",
+			Value: "",
+		},
+		&cli.BoolFlag{
+			Name: "createuser",
+			Value: false,
+		},
+		&cli.BoolFlag{
+			Name: "createsubject",
+			Value: false,
+		},
+	}
+
 	app.Commands = []*cli.Command{
 		{
 			Name: "login",
 			Aliases: []string{"l"},
 			Usage: "Used to login",
 			UsageText: "Other text",
+			Action: handleLogin,
+		},
+		{
+			Name: "insert",
+			Aliases: []string{"i"},
+			Usage: "Insert Records from .xlsx file",
+			Flags: insertFlags,
 			Action: func(ctx *cli.Context) error {
+				fileName := string(ctx.Args().Get(0))
+				sheetName := string(ctx.Args().Get(1))
 				if ctx.NArg() == 0 {
-					return fmt.Errorf("invalid username")
+					return fmt.Errorf("please provide file name")
 				}
-				password, err := readPassword()
-				if err != nil {
-					fmt.Println(err.Error())
-					return nil
-				}
-				postBody, err := json.Marshal(map[string]string {
-					"email": ctx.Args().Get(0),
-					"password": password,
-				})
+				f, err := excelize.OpenFile(fileName)
 				if err != nil {
 					return err
 				}
-				responseBody := bytes.NewBuffer(postBody)
-				resp, err := http.Post(env["WEBSITE"]+"/authorize/admin", "application/json", responseBody)
+				defer func() {
+					if err := f.Close(); err != nil {
+						fmt.Println("File not closed")
+					}
+				}()
+				rows, err := f.GetRows(sheetName)
 				if err != nil {
 					return err
 				}
-				defer resp.Body.Close()
-				var data map[string]interface{}
-				err = json.NewDecoder(resp.Body).Decode(&data)
-				if err != nil {
-					return err
+				for idx, row := range rows {
+					postBody, err := json.Marshal(map[string]string {
+						"entryNumber": row[0],
+						"grade": row[1],
+						"subjectCode": func() string {
+							if len(ctx.String("subjectcode")) == 0 {
+								return row[2]
+							} else {
+								return ctx.String("subjectcode")
+							}
+						}(),
+						"semester": func() string {
+							if len(ctx.String("semester")) == 0 {
+								return row[3]
+							} else {
+								return ctx.String("semester")
+							}
+						}(),
+					})
+					responseBody := bytes.NewBuffer(postBody)
+					if err != nil {
+						f.SetCellValue(sheetName, "E"+fmt.Sprint(idx+1), "Failed")
+						return err
+					}
+					client := &http.Client{}
+					req, err := http.NewRequest("POST", env["WEBSITE"]+"/admin/records/single", responseBody)
+					if err != nil {
+						f.SetCellValue(sheetName, "E"+fmt.Sprint(idx+1), "Failed")
+						return err
+					}
+					req.Header.Add("Authorization", "Bearer " + env["TOKEN"])
+					req.Header.Set("Content-Type", "application/json")
+					res, err := client.Do(req)
+					if err != nil {
+						f.SetCellValue(sheetName, "E"+fmt.Sprint(idx+1), "Failed")
+						return err
+					}
+					defer res.Body.Close()
+					var data map[string]interface{}
+					err = json.NewDecoder(res.Body).Decode(&data)
+					if err != nil {
+						f.SetCellValue(sheetName, "E"+fmt.Sprint(idx+1), "Failed")
+						return err
+					}
+					f.SetCellValue(sheetName, "E"+fmt.Sprint(idx+1), data["msg"])
 				}
-				msg, ok := data["msg"].(string)
-				if ok {
-					return fmt.Errorf(msg)
-				}
-				env["TOKEN"] = data["token"].(string)
-				godotenv.Write(env, ".env")
-				fmt.Printf("Authenticated!")
+				f.SaveAs(fileName)
 				return nil
 			},
 		},
@@ -98,3 +159,42 @@ func readPassword() (string, error) {
 	}
 	return password, err
 }
+
+func handleLogin(ctx *cli.Context) error {
+	if ctx.NArg() == 0 {
+		return fmt.Errorf("invalid username")
+	}
+	password, err := readPassword()
+	if err != nil {
+		fmt.Println(err.Error())
+		return nil
+	}
+	postBody, err := json.Marshal(map[string]string {
+		"email": ctx.Args().Get(0),
+		"password": password,
+	})
+	if err != nil {
+		return err
+	}
+	responseBody := bytes.NewBuffer(postBody)
+	res, err := http.Post(env["WEBSITE"]+"/authorize/admin", "application/json", responseBody)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+	var data map[string]interface{}
+	err = json.NewDecoder(res.Body).Decode(&data)
+	if err != nil {
+		return err
+	}
+	msg, ok := data["msg"].(string)
+	if ok {
+		return fmt.Errorf(strings.ToLower(msg))
+	}
+	env["TOKEN"] = data["token"].(string)
+	godotenv.Write(env, ".env")
+	fmt.Printf("Authenticated. ")
+	fmt.Println("The login expires after 10 hours!")
+	return nil
+}
+
